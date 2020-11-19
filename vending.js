be50497dcc4c837c507377a47ab4200c5889eb88
Moe Itayem <mitayem232@gmail.com>
2020-11-19 15:51:31 -0500

    added Function for Total

--- vending.js
+++ vending.js
@@ -5,3 +5,24 @@ totalPaid = 0;
 const currency_quarter = 0.25;
 const currency_dime = 0.01;
 const currency_nickel = 0.05;
+
+
+
+function getTotal(){
+    var currency_quarters = Number(document.getElementById("quarters").value);
+    var currency_dimes = Number(document.getElementById("dimes").value);
+    var currency_nickels = Number(document.getElementById("nickels").value);
+
+    if(currency_quarters > 0 ) {
+        currency_quarters = currency_quarters * currency_quarter;
+    }
+    if(currency_dimes > 0 ) {
+        currency_dimes = currency_dimes * currency_dime;
+    }
+    if(currency_nickels > 0 ) {
+        currency_nickels = currency_nickels * currency_nickel;
+    }
+
+totalPaid = currency_quarters + currency_dimes + currency_nickels;
+return totalPaid.toFixed(2);
+}
