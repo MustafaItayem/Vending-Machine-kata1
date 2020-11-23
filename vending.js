515ee507acb35df556105f530e8922c46e138028
Moe Itayem <mitayem232@gmail.com>
2020-11-22 22:45:22 -0500

    Able to buy from Machine

--- vending.js
+++ vending.js
@@ -1,7 +1,12 @@
 var change = 0;
 var moneyInserted = 0;
 var msg = "";
- 
+var items = ["Cola", "Chips", "Candy"];
+const price  = 1.00;
+// var chips = ["Chips"];
+// const price  = 0.50;
+// var candy = ["Candy"];
+// const price  = 0.65;
 var messageEL = document.getElementById("message");
 
 var totalPaid = 0;
@@ -63,3 +68,45 @@ function cancel() {
     }
     
 } 
+
+function calChange(){
+    var tempChange = 0;
+
+
+    if(getTotal() != 0){
+        return (tempChange = (getTotal() - price).toFixed(2));
+    
+    }
+return tempChange.toFixed(2);
+
+}
+
+function dispenseItems(items) {
+    messageEL.innerHTML = "";
+    change = 0;
+
+    var selectedItems = items[items];
+
+
+    change = calChange();
+
+    if (change < 0){
+        msg = "You did not pay enough. $" + totalPaid.toFixed(2) + " has been returend to the coin return"
+        totalPaid = 0;
+        change = 0;
+        clearForm();
+        clearTally();
+
+        messageEL.innerHTML = msg;
+    }else if (change > 0){
+        msg = selectedItems + " has been dispensed. $" + change + " has been returned to the cain return."
+        totalPaid = 0;
+        change = 0;
+        clearForm();
+        clearTally();
+
+        messageEL.innerHTML = msg;
+    }
+
+
+}
